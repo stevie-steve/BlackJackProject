5cce7b00513986bb31a87e4f427fce8e386b42fe
stevie-steve <sharpsrjr@gmail.com>
2020-04-10 11:23:04 -0600

    initial commit

---
+++ src/com/skilldistillery/blackjack/Deck.java
@@ -0,0 +1,31 @@
+package com.skilldistillery.blackjack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Deck {
+List <Card> cards = new ArrayList<>();
+
+//need a deck
+//must create 4 cards lists of each suit?
+
+private List<Card> deck(){{
+	super(); 
+	this.deck = deck;
+}
+
+public int checkDeckSize() {
+	
+	return cards.size(); //num cards in deck .size
+}
+
+public Card dealCard(){
+	
+	return cards.remove(0); //removes card (.remove)
+}
+
+public void shuffle() {
+	return 0; //collections.shuffle()
+	
+}
+}
